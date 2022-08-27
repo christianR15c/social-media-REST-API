4789eae44433fdd67e46b64d92b6f146f60198a9
christian <habinezac15@gmail.com>
2022-08-27 10:14:56 +0200

    posts crud operation added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ app.use(morgan('common'))
 
 app.use('/api/users', userRoute)
 app.use('/api/', authRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(8800, () => {
     mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
